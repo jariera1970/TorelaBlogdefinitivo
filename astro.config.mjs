70541f808078f028680011613fa2eb7cc4033a7e
Jose Antonio <joseantoniorierasanchez@gmail.com>
2026-03-31 15:48:38 +0200

    refactor: migrar sitio a SSG estatico

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,12 @@
 
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	output: 'server',
-	adapter: vercel(),
+	output: 'static',
 	server: {
 		host: '127.0.0.1',
 		port: 4321,
